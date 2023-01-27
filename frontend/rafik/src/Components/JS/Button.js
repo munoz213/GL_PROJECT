444f651b0b58ccb737c466ae8cef25124cca7927
triiix <58532893+triiix@users.noreply.github.com>
2023-01-27 23:47:04 +0100

    Delete frontend/rafik/src/Components directory

--- frontend/rafik/src/Components/JS/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-
-
-export default function Button({to, text}) {
-  return (
-    <Link to={to} className="main-button">{text}</Link>
-  )
-}
