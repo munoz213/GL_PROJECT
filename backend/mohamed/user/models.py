e95c3f2fd270fcb70710a11027197c3448cc7e48
munoz213 <118865232+munoz213@users.noreply.github.com>
2023-01-27 18:30:03 -1100

    mohamed

--- backend/mohamed/user/models.py
+++ backend/mohamed/user/models.py
@@ -9,8 +9,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True, nullable=False)
     name = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(20), nullable = False, unique =True)
-    password = db.Column(db.String(80), nullable=False)
-    phone = db.Column(db.String(10), nullable=False, unique=True)
+ 
 
 
 ma = Marshmallow(app)
