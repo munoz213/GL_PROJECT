b1a0cd953d8ec79bfc0e933657faa72b8c4305f0
triiix <58532893+triiix@users.noreply.github.com>
2023-01-30 00:28:34 +0100

    Delete frontend/rafik/src directory

--- frontend/rafik/src/Components/JS/HomeTop.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function HomeTop() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
--- frontend/rafik/src/Components/JS/PostCourse.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function PostCourse() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
